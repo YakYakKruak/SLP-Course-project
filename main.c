13b187715bdbcff2e2f78e844f502ce457c69e2a
YakYakKruak <golovanev98@mail.ru>
2017-11-26 01:17:11 +0300

    idk

---
+++ main.c
@@ -0,0 +1,5 @@
+//
+// Created by yakov on 26.11.17.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by yakov on 26.11.17.
+//
+
+#ifndef SLP_COURSE_PROJECT_MAIN_H
+#define SLP_COURSE_PROJECT_MAIN_H
+
+#endif //SLP_COURSE_PROJECT_MAIN_H
